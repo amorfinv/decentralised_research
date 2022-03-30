63f82ba747a0dc75f16ead88be27b32b222511ee
nipatrin <nipatrin@gmail.com>
2022-03-30 13:00:16 +0300

    Flow sectors

---
+++ flow_control/Edges2FlowGroupsAssignement.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 30 12:09:49 2022
+
+@author: nipat
+"""
+
+import osmnx as ox
+import geopandas as gd
+import shapely.geometry
+import pandas as pd
+
+
+G = ox.io.load_graphml('whole_vienna/gis/finalized_graph.graphml')
+
+nodes, edges = ox.graph_to_gdfs(G)
+
+edges=edges.to_crs(epsg='32633')
+
+flow_grid=gd.read_file("grid.gpkg")
+
+edges2flowgroup_dict={}
+
+for ii, edg in edges.iterrows():
+    edge_index=edg.index
+     
+    #edges_indices=list(flow_grid.sindex.intersection(edges.loc[ii]["geometry"].bounds))
+    edges_indices=list(flow_grid.sindex.query(edges.loc[ii]["geometry"],predicate="intersects"))
+    if edges_indices==[]:
+        print("No flow group match")
+    elif len(edges_indices)>1:
+        lens_list=[]
+        for i in edges_indices:
+            grid_poly=flow_grid.loc[i]["geometry"]
+
+            intersection_points=grid_poly.intersection(edges.loc[ii]["geometry"])
+            line_seg_len=intersection_points.length
+            lens_list.append(line_seg_len)
+            if line_seg_len==0.0:
+                print(intersection_points)
+                print(i,ii)
+        max_len=max(lens_list)
+        flow_index=lens_list.index(max_len)
+        edges_indices=[flow_index]
+            
+        
+    edges2flowgroup_dict[ii]=edges_indices[0]
+    
+    
+edges['Flow_sectors'] = pd.Series(edges2flowgroup_dict)   
+
+edges=edges.to_crs(epsg='4326')
+
+g=ox.graph_from_gdfs(nodes,edges)
+
+ox.save_graphml(g,"graph_flow_sectors.graphml")
+
+ox.save_graph_geopackage(g,"graph_flow_sectors.gpkg",directed=True)

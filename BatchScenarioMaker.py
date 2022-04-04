7cae68d2cba6b99c26945eb55923421986c2bafc
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2022-04-04 16:17:35 +0200

    add baseline scenarios

--- BatchScenarioMaker.py
+++ BatchScenarioMaker.py
@@ -12,7 +12,8 @@ scenario_folders = ['M2_scenarios/',
                     'airspace_structure_study/1_to_1_scenarios/',
                     'heading_layer_allocation_study/head_alloc_scenarios/',
                     'heading_layer_allocation_study/heading_alloc_scenarios_no_flow/',
-                    'heading_based_cr/hdg_cr_scenarios/']
+                    'heading_based_cr/hdg_cr_scenarios/',
+                    'baseline_scenarios/']
 
 
 # set location of places that bluesky should search for all the blueksy scenarios

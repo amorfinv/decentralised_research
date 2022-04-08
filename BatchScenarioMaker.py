8ebecd0069477a7b2c1c11949c4aba3453efd5c9
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2022-04-08 11:45:25 +0200

    update batches

--- BatchScenarioMaker.py
+++ BatchScenarioMaker.py
@@ -14,7 +14,8 @@ scenario_folders = ['M2_scenarios/',
                     'heading_layer_allocation_study/heading_alloc_scenarios_no_flow/',
                     'heading_based_cr/hdg_cr_scenarios/',
                     'baseline_scenarios/',
-                    'flow_control/grid_sector_scenarios']
+                    # 'flow_control/grid_sector_scenarios'
+                    ]
 
 
 # set location of places that bluesky should search for all the blueksy scenarios
@@ -29,12 +30,13 @@ for scenario_folder in scenario_folders:
     scenario_files += [x for x in scenario_folder_files if x.startswith('Flight_')]
 
 # split the scenarios
-very_low_scenarios = [file for file in scenario_files if 'very_low' in file]
-low_scenarios = [file for file in scenario_files if 'low' in file and 'very' not in file]
-medium_scenarios = [file for file in scenario_files if 'medium' in file]
-high_scenarios = [file for file in scenario_files if 'high' in file]
-ultra_scenarios = [file for file in scenario_files if 'ultra' in file]
+very_low_scenarios = [file for file in scenario_files if '_very_low_' in file]
+low_scenarios = [file for file in scenario_files if '_low_' in file and '_very_' not in file]
+medium_scenarios = [file for file in scenario_files if '_medium_' in file]
+high_scenarios = [file for file in scenario_files if '_high_' in file]
+ultra_scenarios = [file for file in scenario_files if '_ultra_' in file]
 
+# %%
 # assemble scenarios into two batches
 batch_1 = very_low_scenarios + low_scenarios + medium_scenarios
 batch_2 = high_scenarios + ultra_scenarios

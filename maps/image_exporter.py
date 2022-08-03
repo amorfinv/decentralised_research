fa348c048d8746db16b796044bc49422b018f642
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2022-08-03 13:52:44 +0200

    update map making

--- maps/image_exporter.py
+++ maps/image_exporter.py
@@ -1,115 +1,141 @@
 
 ### run code inside QGIS python console for ease of use
 import os
+import re
+from copy import copy
 
 # directores where data is stored
 # image directory
-image_dir = "/Users/andresmorfin/Desktop/M2/m2_heat_maps/images"
+image_dir = '/Users/localadmin/Desktop/andresmorfin/decentralised_research/maps/images'
 
 # get the style file
-style_dir = '/Users/andresmorfin/Desktop/M2/m2_heat_maps/styles'
+style_dir = '/Users/localadmin/Desktop/andresmorfin/decentralised_research/maps/styles'
 
 # geotiff directory
-geotiff_dir = '/Users/andresmorfin/Desktop/M2/m2_heat_maps/geotif'
+geotiff_dir = '/Users/localadmin/Desktop/andresmorfin/decentralised_research/maps/geotif'
 
 # list the contents (remove hidden files)
 geotiff_files = os.listdir(geotiff_dir)
-geotiff_files = [f for f in geotiff_files if not f.startswith('.')]
+
+# remove anything that does not start with CONFLOG
+# geotiff_files = [f for f in geotiff_files if f.startswith('CONFLOG')]
+
+# now check inside images for the same files
+image_files = os.listdir(image_dir)
 
 # acces qgis project
 project = QgsProject.instance()
 
 #  loop through geotiff files and export
 for geo_tif in geotiff_files:
-    # get the raster layer path
-    rasterlayer_path = os.path.join(geotiff_dir, geo_tif)
 
-    # check which density layer we are one
-    current_density = geo_tif.split('_')[2]
-    if current_density == 'very':
-        current_density = 'very_low'
+    pdf_file = f'{geo_tif[:-4]}.pdf'
 
-    # logtype
-    log_type = geo_tif.split('_')[1]
+    if pdf_file in image_files:
+        print(f'{pdf_file} already exists')
+        continue
     
-    # find style path
-    style_path = os.path.join(style_dir, f'{log_type}_{current_density}.qml')
+    print(f'Attempting {geo_tif}')
 
-    # load the layer into the project amd give it a style
-    layer = QgsRasterLayer(rasterlayer_path)
-    layer.loadNamedStyle(style_path)
-    project.addMapLayer(layer)
+    try:
+        # get the raster layer path
+        rasterlayer_path = os.path.join(geotiff_dir, geo_tif)
 
-    # access the project layout manager and create a new layout
-    manager = project.layoutManager()
-    layout = QgsPrintLayout(project)
-    layout.initializeDefaults()
-    layout.setUnits(QgsUnitTypes.LayoutMillimeters)
-    page = layout.pageCollection().pages()[0]
+        # check which density layer we are one
+        current_density = geo_tif.split('_')[1]
+        if current_density == 'very':
+            current_density = 'very_low'
 
-    # give it a paper size and a name
-    paperSize = 280, 300
-    page.setPageSize(QgsLayoutSize(paperSize[0],paperSize[1]))
-    layout.setName(f'{geo_tif[:-4]}')
+        # logtype
+        log_type = geo_tif.split('_')[0]
 
-    # add the layout to project layout manager
-    manager.addLayout(layout)
+        # find style path
+        style_path = os.path.join(style_dir, f'{log_type}_{current_density}.qml')
 
-    # Map
-    map = QgsLayoutItemMap(layout)
-    map.setRect(QRectF(20, 20, 200, 200))  # The Rectangle will be overridden below
+        # load the layer into the project amd give it a style
+        layer = QgsRasterLayer(rasterlayer_path)
+        layer.loadNamedStyle(style_path)
+        project.addMapLayer(layer)
 
-    # Extent
-    ext = layer.extent()
-    rectangle = QgsRectangle(593213.4382722532609478,5331407, 609337,5347966.6254693605005741)
-    map.setExtent(ext)
-    layout.addLayoutItem(map)
+        # access the project layout manager and create a new layout
+        manager = project.layoutManager()
+        layout = QgsPrintLayout(project)
+        layout.initializeDefaults()
+        layout.setUnits(QgsUnitTypes.LayoutMillimeters)
+        page = layout.pageCollection().pages()[0]
 
-    # Move & Resize
-    # Demo of map origin and map size in mixed units
-    map.attemptMove(QgsLayoutPoint(15, 15, QgsUnitTypes.LayoutMillimeters))
-    map.attemptResize(QgsLayoutSize(250,  250, QgsUnitTypes.LayoutMillimeters))
+        # give it a paper size and a name
+        paperSize = 280, 300
+        page.setPageSize(QgsLayoutSize(paperSize[0],paperSize[1]))
+        layout.setName(f'{geo_tif[:-4]}')
 
-    # add a title
-    label = QgsLayoutItemLabel(layout)
-    label.setText(f"{geo_tif[:-4]}")
-    label.attemptMove(QgsLayoutPoint(101.151, 4.922, QgsUnitTypes.LayoutMillimeters))
-    label.setFont(QFont('Arial', 14))
-    label.adjustSizeToText()
-    label.setHAlign(Qt.AlignCenter)
-    layout.addLayoutItem(label)
+        # add the layout to project layout manager
+        manager.addLayout(layout)
 
-    # legend
-    legend = QgsLayoutItemLegend(layout)
-    legend.setAutoUpdateModel(False)
-    layout.addLayoutItem(legend)
-    root = QgsLayerTree()
-    root.addLayer(layer)
-    legend.model().setRootGroup(root)
-    # legend.setLinkedMap(map) # map is an instance of QgsLayoutItemMap
+        # Map
+        map = QgsLayoutItemMap(layout)
+        map.setRect(QRectF(20, 20, 200, 200))  # The Rectangle will be overridden below
 
-    legend.setSymbolHeight(15)
-    legend.setSymbolWidth(100.0)
-    legend.attemptMove(QgsLayoutPoint(87.500, 268.017, QgsUnitTypes.LayoutMillimeters))
+        # Extent
+        ext = layer.extent()
+        rectangle = QgsRectangle(593213.4382722532609478,5331407, 609337,5347966.6254693605005741)
+        map.setExtent(ext)
+        layout.addLayoutItem(map)
 
-    # hide the node with label: Band 1 (Gray)
-    model = legend.model()
-    froot = model.rootGroup().findLayer(layer)
+        # Move & Resize
+        # Demo of map origin and map size in mixed units
+        map.attemptMove(QgsLayoutPoint(15, 15, QgsUnitTypes.LayoutMillimeters))
+        map.attemptResize(QgsLayoutSize(250,  250, QgsUnitTypes.LayoutMillimeters))
 
-    if isinstance(layer, QgsRasterLayer):
-        nodes = model.layerLegendNodes(froot)
-        if nodes[0].data(0) == 'Band 1 (Gray)':
-            indexes = list(range(1, len(nodes)))
-            QgsMapLayerLegendUtils.setLegendNodeOrder(froot, indexes)
-            model.refreshLayerLegend(froot)
+        # add a title
+        label = QgsLayoutItemLabel(layout)
+        label.setText(f"{geo_tif[:-4]}")
+        label.attemptMove(QgsLayoutPoint(101.151, 4.922, QgsUnitTypes.LayoutMillimeters))
+        label.setFont(QFont('Arial', 14))
+        label.adjustSizeToText()
+        label.setHAlign(Qt.AlignCenter)
+        layout.addLayoutItem(label)
 
-    # export
-    exporter = QgsLayoutExporter(layout)
-    pdf_settings = exporter.PdfExportSettings() #dpi?
-    exporter.exportToPdf(os.path.join(image_dir, f'{geo_tif[:-4]}.pdf'), pdf_settings)
+        # legend
+        legend = QgsLayoutItemLegend(layout)
+        legend.setAutoUpdateModel(False)
+        layout.addLayoutItem(legend)
+        root = QgsLayerTree()
+        root.addLayer(layer)
+        legend.model().setRootGroup(root)
+        # legend.setLinkedMap(map) # map is an instance of QgsLayoutItemMap
 
-    # remove the layer from the project
-    QgsProject.instance().removeMapLayer(layer)
+        legend.setSymbolHeight(15)
+        legend.setSymbolWidth(100.0)
+        legend.attemptMove(QgsLayoutPoint(87.500, 268.017, QgsUnitTypes.LayoutMillimeters))
 
-    # remove the layout from the project
-    manager.removeLayout(layout)
+        # hide the node with label: Band 1 (Gray)
+        model = legend.model()
+        froot = model.rootGroup().findLayer(layer)
+
+        if isinstance(layer, QgsRasterLayer):
+            nodes = model.layerLegendNodes(froot)
+            if nodes[0].data(0) == 'Band 1 (Gray)':
+                indexes = list(range(1, len(nodes)))
+                QgsMapLayerLegendUtils.setLegendNodeOrder(froot, indexes)
+                model.refreshLayerLegend(froot)
+
+        # export
+        exporter = QgsLayoutExporter(layout)
+        pdf_settings = exporter.PdfExportSettings() #dpi?
+        exporter.exportToPdf(os.path.join(image_dir, f'{geo_tif[:-4]}.pdf'), pdf_settings)
+
+        # remove the layer from the project
+        QgsProject.instance().removeMapLayer(layer)
+
+        # remove the layout from the project
+        manager.removeLayout(layout)
+        
+        print(f'Succesfully worked for {geo_tif}')
+
+    except RuntimeError:
+        print('---------------------')
+        print(f'Failed for {geo_tif}')
+        print('---------------------')
+        break
+        

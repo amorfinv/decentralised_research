ccbee31dd49ba00fbb35ee764d7244aca77fb3fa
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2022-08-11 17:30:54 +0200

    cleanup log parser

--- conflict_study/logparser.py
+++ conflict_study/logparser.py
@@ -180,57 +180,11 @@ def conflog(scenario_list, dataset_name, parse_args):
         # look at column "AIRSPACETYPE1" and "AIRSPACETYPE2". Only keep the rows if they are both "constrained"
         df = df[(df['AIRSPACETYPE1'] == 'constrained') & (df['AIRSPACETYPE2'] == 'constrained')]
 
-        # go through layertype1 and layertype2 and check if one is a NAN, if so replace the value with 'S'
-        df['LAYERTYPE1'] = df['LAYERTYPE1'].fillna('S')
-        df['LAYERTYPE2'] = df['LAYERTYPE2'].fillna('S')
+        # Filter logs and get a layer type count
+        layer_type_confs = filerlogs_for_layertype(df, concept, density)
 
-        # look at column "LAYERTYPE1" and "LAYERTYPE2" and sort them depending on the values
-        # LAYERTYPE1 = "C" and LAYERTYPE2 = "C" -> "C-C"
-        # LAYERTYPE1 = "S" and LAYERTYPE2 = "S" -> "S-S"
-        # LAYERTYPE1 = "T" and LAYERTYPE2 = "T" -> "T-T"
-        # LAYERTYPE1 = "F" and LAYERTYPE2 = "F" -> "F-F"
-        # LAYERTYPE1 = "C" and LAYERTYPE2 = "T" -> "C-T"
-        # LAYERTYPE1 = "T" and LAYERTYPE2 = "C" -> "C-T"
-        # LAYERTYPE1 = "C" and LAYERTYPE2 = "F" -> "C-F"
-        # LAYERTYPE1 = "F" and LAYERTYPE2 = "C" -> "C-F"
-        # LAYERTYPE1 = "F" and LAYERTYPE2 = "T" -> "F-T"
-        # LAYERTYPE1 = "T" and LAYERTYPE2 = "F" -> "F-T"
-        # LAYERTYPE1 = "S" and LAYERTYPE2 = "C" -> "C-S"
-        # LAYERTYPE1 = "C" and LAYERTYPE2 = "S" -> "C-S"
-        # LAYERTYPE1 = "S" and LAYERTYPE2 = "T" -> "S-T"
-        # LAYERTYPE1 = "T" and LAYERTYPE2 = "S" -> "S-T"
-        # LAYERTYPE1 = "S" and LAYERTYPE2 = "F" -> "F-S"
-        # LAYERTYPE1 = "F" and LAYERTYPE2 = "S" -> "F-S"
-        df['layertype'] = df.apply(lambda row: row['LAYERTYPE1'] + '-' + row['LAYERTYPE2'] if row['LAYERTYPE1'] < row['LAYERTYPE2'] else row['LAYERTYPE2'] + '-' + row['LAYERTYPE1'], axis=1)
-
-        # get the layer type conflict counts
-        layer_type_confs = df.groupby(by=['scenario','layertype']).size()
-
-        # make into a dataframe and make index into columns and name column count
-        layer_type_confs = layer_type_confs.to_frame().reset_index().rename(columns={0:'count'})
-        layer_type_confs['concept'] = concept
-        layer_type_confs['density'] = density
-
-        # check if any of these are missing per scenario and add it with a value of 0
-        conflict_types = ['C-C', 'S-S', 'T-T', 'F-F', 'C-T', 'C-F', 'F-T', 'C-S', 'S-T', 'F-S']
-        for scenario in df['scenario'].unique():
-            for conflict_type in conflict_types:
-                if conflict_type not in layer_type_confs[layer_type_confs['scenario'] == scenario]['layertype'].unique():
-                    layer_type_confs = layer_type_confs.append({'scenario': scenario, 'layertype': conflict_type, 'count': 0, 'concept': concept, 'density': density}, ignore_index=True)
-
-        # add a new column called 'repetition' that takes the 'scenario' string and splits it on the underscore and takes the last value
-        layer_type_confs['repetition'] = layer_type_confs['scenario'].apply(lambda x: x.split('_')[-2])
-        
-        # another interesting study would be to look at the altitude differences between conflicts
-        df['ALT_DIFF'] = df.apply(lambda row: abs(row['ALT1'] - row['ALT2']), axis=1)
-        
-        # create some altitude bins to place alt-diff values in 0-9.144, 9.144-18.288, 18.288-27.432, 27.432-36.576, 36.576-45.72, 45.72-150
-        df['altitudebins'] = pd.cut(df['ALT_DIFF'], bins=[0, 9.144, 18.288, 27.432, 36.576, 45.72, 150], labels=['0L', '1L', '2L', '3L', '4L', '>4L'])
-        
-        altitude_confs = df.groupby(by=['scenario','altitudebins']).size()
-        altitude_confs = altitude_confs.to_frame().reset_index().rename(columns={0:'count'})
-        altitude_confs['concept'] = concept
-        altitude_confs['density'] = density
+        # Filter logs and get an altitude bin count
+        altitude_confs = filterlogs_for_altbins(df, concept, density)
 
         return layer_type_confs, altitude_confs
 
@@ -245,7 +199,7 @@ def loslog(scenario_list, gpkg_name, gpkg_args):
     loslog_files = [os.path.join('results',f) for f in scenario_list if 'LOSLOG' in f]
 
     # read the files and skip the first 9 rows
-    header_columns = ['exittime','starttime','timemindist','ACID1','ACID2','LAT1','LON1','ALT1','LAT2','LON2','ALT2','DIST']
+    header_columns = ['exittime','starttime','timemindist','ACID1','ACID2','LAT1','LON1','ALT1','LAT2','LON2','ALT2','DIST', 'AIRSPACETYPE1', 'AIRSPACETYPE2', 'LAYERTYPE1', 'LAYERTYPE2', 'EDGEID1', 'EDGEID2']
 
     print(f'[green]Parsing {gpkg_name}...')
     try:
@@ -258,15 +212,81 @@ def loslog(scenario_list, gpkg_name, gpkg_args):
 
         # convert coords to numpy array
         # convert to geodataframe
-        gdf = gpd.GeoDataFrame(df, geometry=df.apply(lambda row: MultiPoint([(row['LON1'], row['LAT1']), (row['LON2'], row['LAT2'])]), axis=1), crs='epsg:4326')    
+        # gdf = gpd.GeoDataFrame(df, geometry=df.apply(lambda row: MultiPoint([(row['LON1'], row['LAT1']), (row['LON2'], row['LAT2'])]), axis=1), crs='epsg:4326')
 
-        # convert to epsg 32633
-        gdf = gdf.to_crs(epsg=32633)
+        # look at column "AIRSPACETYPE1" and "AIRSPACETYPE2". Only keep the rows if they are both "constrained"
+        df = df[(df['AIRSPACETYPE1'] == 'constrained') & (df['AIRSPACETYPE2'] == 'constrained')]
 
-        # convert to a geopackage
-        gpkg_fpath = os.path.join('gpkgs', gpkg_name)
-        gdf.to_file(gpkg_fpath + '.gpkg', driver='GPKG')
+        # go through layertype1 and layertype2 and check if one is a NAN, if so replace the value with 'S'
+        df['LAYERTYPE1'] = df['LAYERTYPE1'].fillna('S')
+        df['LAYERTYPE2'] = df['LAYERTYPE2'].fillna('S')
 
+        cc
     except ValueError:
         print('[red]Problem with these files:')
         print(scenario_list)
+
+
+def filterlogs_for_altbins(df: pd.DataFrame, concept: str, density: str)-> pd.DataFrame:
+    """
+    Filter logs and get a layer type count
+    """
+
+    # another interesting study would be to look at the altitude differences between conflicts
+    df['ALT_DIFF'] = df.apply(lambda row: abs(row['ALT1'] - row['ALT2']), axis=1)
+    
+    # create some altitude bins to place alt-diff values in 0-9.144, 9.144-18.288, 18.288-27.432, 27.432-36.576, 36.576-45.72, 45.72-150
+    df['altitudebins'] = pd.cut(df['ALT_DIFF'], bins=[0, 9.144, 18.288, 27.432, 36.576, 45.72, 150], labels=['0L', '1L', '2L', '3L', '4L', '>4L'])
+    
+    altitude_confs = df.groupby(by=['scenario','altitudebins']).size()
+    altitude_confs = altitude_confs.to_frame().reset_index().rename(columns={0:'count'})
+    altitude_confs['concept'] = concept
+    altitude_confs['density'] = density
+
+def filerlogs_for_layertype(df: pd.DataFrame, concept: str, density: str) -> pd.DataFrame:
+    '''
+    Code to filter CONFLOG OR LOSLOG files for layer type conflicts
+    '''
+    # go through layertype1 and layertype2 and check if one is a NAN, if so replace the value with 'S'
+    df['LAYERTYPE1'] = df['LAYERTYPE1'].fillna('S')
+    df['LAYERTYPE2'] = df['LAYERTYPE2'].fillna('S')
+
+    # look at column "LAYERTYPE1" and "LAYERTYPE2" and sort them depending on the values
+    # LAYERTYPE1 = "C" and LAYERTYPE2 = "C" -> "C-C"
+    # LAYERTYPE1 = "S" and LAYERTYPE2 = "S" -> "S-S"
+    # LAYERTYPE1 = "T" and LAYERTYPE2 = "T" -> "T-T"
+    # LAYERTYPE1 = "F" and LAYERTYPE2 = "F" -> "F-F"
+    # LAYERTYPE1 = "C" and LAYERTYPE2 = "T" -> "C-T"
+    # LAYERTYPE1 = "T" and LAYERTYPE2 = "C" -> "C-T"
+    # LAYERTYPE1 = "C" and LAYERTYPE2 = "F" -> "C-F"
+    # LAYERTYPE1 = "F" and LAYERTYPE2 = "C" -> "C-F"
+    # LAYERTYPE1 = "F" and LAYERTYPE2 = "T" -> "F-T"
+    # LAYERTYPE1 = "T" and LAYERTYPE2 = "F" -> "F-T"
+    # LAYERTYPE1 = "S" and LAYERTYPE2 = "C" -> "C-S"
+    # LAYERTYPE1 = "C" and LAYERTYPE2 = "S" -> "C-S"
+    # LAYERTYPE1 = "S" and LAYERTYPE2 = "T" -> "S-T"
+    # LAYERTYPE1 = "T" and LAYERTYPE2 = "S" -> "S-T"
+    # LAYERTYPE1 = "S" and LAYERTYPE2 = "F" -> "F-S"
+    # LAYERTYPE1 = "F" and LAYERTYPE2 = "S" -> "F-S"
+    df['layertype'] = df.apply(lambda row: row['LAYERTYPE1'] + '-' + row['LAYERTYPE2'] if row['LAYERTYPE1'] < row['LAYERTYPE2'] else row['LAYERTYPE2'] + '-' + row['LAYERTYPE1'], axis=1)
+
+    # get the layer type conflict counts
+    layer_type_confs = df.groupby(by=['scenario','layertype']).size()
+
+    # make into a dataframe and make index into columns and name column count
+    layer_type_confs = layer_type_confs.to_frame().reset_index().rename(columns={0:'count'})
+    layer_type_confs['concept'] = concept
+    layer_type_confs['density'] = density
+
+    # check if any of these are missing per scenario and add it with a value of 0
+    conflict_types = ['C-C', 'S-S', 'T-T', 'F-F', 'C-T', 'C-F', 'F-T', 'C-S', 'S-T', 'F-S']
+    for scenario in df['scenario'].unique():
+        for conflict_type in conflict_types:
+            if conflict_type not in layer_type_confs[layer_type_confs['scenario'] == scenario]['layertype'].unique():
+                layer_type_confs = layer_type_confs.append({'scenario': scenario, 'layertype': conflict_type, 'count': 0, 'concept': concept, 'density': density}, ignore_index=True)
+
+
+    # add a new column called 'repetition' that takes the 'scenario' string and splits it on the underscore and takes the last value
+    layer_type_confs['repetition'] = layer_type_confs['scenario'].apply(lambda x: x.split('_')[-2])
+
+    return layer_type_confs

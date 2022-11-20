aee49011e1deceda39ef4232b7f1acc02073f137
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2022-11-20 15:49:43 +0100

    Update logparser.py

--- maps/logparser.py
+++ maps/logparser.py
@@ -4,9 +4,12 @@ from rich.progress import track
 import pandas as pd
 from datetime import timedelta, datetime
 from shapely.geometry import Point, MultiPoint
-import functools
+import osmnx as ox
+import networkx as nx
 import geopandas as gpd
 import numpy as np
+from itertools import islice
+from copy import deepcopy
 
 # ignore warnings with the way the gdf is created
 import warnings
@@ -70,65 +73,94 @@ def reglog(scenario_list, gpkg_name, gpkg_args):
     # each time stamp should have four entries
     time_stamp_entries = dict()
 
+    # frest reglog creates general log
     print(f'[green]Parsing {gpkg_name}')
-    try:
 
+    # read the grahpml of streets
+    G = ox.load_graphml('other_data/finalized_graph.graphml')
+    edge_gdf = ox.graph_to_gdfs(G, nodes=False)
+    edge_gdf['edge_id'] = edge_gdf.index
+    edge_geometry = nx.get_edge_attributes(G, "geometry")
+
+    try:
         for idx, filepath in enumerate(reglog_files):
             day = idx + 1
             date = datetime.strptime(f'2022-01-{day}',"%Y-%m-%d")
-
-            with open(filepath) as f:
-                for line_num, line in enumerate(f):
-                    if line_num < 9:
-                        continue
-                    header_id = (line_num - 9) % 5
-
-                    # get the time
-                    sec_sim = float(line.split(',')[0])
-                    time_stamp = str(date + timedelta(seconds=sec_sim))
-                    if header_id == 0:
-                        # make a dictionary wth values for each header_column
-                        data[time_stamp] = {header_col:[] for header_col in header_2}            
-                        data[time_stamp][header_columns[header_id]] = line.strip('\n').split(',')[1:]
-                        data[time_stamp]['scenario'] = filepath[len('results')+1:] 
-                    elif header_id == 4:
-                        data[time_stamp][header_columns[header_id]] = [i for i in line.strip('\n').split(',')[1:]]
-                    else:
-                        data[time_stamp][header_columns[header_id]] = [float(i) for i in line.strip('\n').split(',')[1:]]
-
+            
+            with open(filepath, 'r') as infile:
+                # skip first 9 lines of file
+                lines_gen = islice(infile, 9, None)
+                
+                # loop through five lines at time
+                slice_size = 5
+                entry = 0
+                for acids, alts, lats, lons, edgeids in zip(*[iter(lines_gen)]*slice_size):
                     
-                    # create a check to see if the data is complete.
-                    time_stamp_entries[time_stamp] = time_stamp_entries.get(time_stamp, 0) + 1
+                    time = float(acids.split(',')[0])
+                    time_stamp = str(date + timedelta(seconds=time))
 
-        # get a dataframe of the entries
-        df_time_entries = pd.DataFrame(time_stamp_entries, index=['count']).T
+                    acids = acids.strip().split(',')[1:]
+                    alts = alts.strip().split(',')[1:]
+                    lats = lats.strip().split(',')[1:]
+                    lons = lons.strip().split(',')[1:]
+                    edgeids = edgeids.strip().split(',')[1:]
+                    edgeids_float = [tuple(map(int, edge.split('-'))) + (0,) for edge in edgeids]
+                    edge_geom = list(map(edge_geometry.get, edgeids_float))
+    
 
-        # convert to pandas dataframe where the index is the data keys and the columns are the header columns
+                    for acidx, acid in enumerate(acids):
+                        data[entry] = {
+                            'time_stamp': time_stamp,
+                            'seconds': time,
+                            'ACID': acid,
+                            'ALT': float(alts[acidx]),
+                            'geometry': Point(float(lons[acidx]), float(lats[acidx])),
+                            'edge_id': edgeids_float[acidx],
+                            'scenario': filepath[32:-4],
+                            'edge_geometry': edge_geom[acidx]
+                            }
+
+                        entry += 1
+
+        # convert to a geodataframe
         df = pd.DataFrame(data).T
-
-        # remove entries from df that have a value less than 4 in the df_time_entries
-        df = df[df_time_entries['count'] >= 4]
-
-        multi_point = np.array([MultiPoint(np.column_stack((x, y))) for x,y in zip(df['LONS'], df['LATS'])], dtype=MultiPoint)
-
-        # remove ACID, LAT, LON, ALT columns
-        df.drop(columns=['ACID','LATS', 'LONS', 'EDGE_ID', 'ALT'], inplace=True)
-        # make a geodataframe from the pandas dataframe
-        gdf = gpd.GeoDataFrame(df, geometry=multi_point, crs='epsg:4326')
-
-        # set the index as a column
-        gdf['date'] = gdf.index
-        gdf['date']= pd.to_datetime(gdf['date'])
-
-        # reset index
-        gdf.reset_index(inplace=True)
-
-        # convert to epsg 32633
+        
+        # save the geometry of the points
+        gdf = gpd.GeoDataFrame(df, crs='epsg:4326')
         gdf = gdf.to_crs(epsg=32633)
-
-        # convert to a geopackage
+        gdf = gdf.drop(columns=['edge_id','edge_geometry'])
         gpkg_fpath = os.path.join('gpkgs', gpkg_name)
-        gdf.to_file(gpkg_fpath + '.gpkg', driver='GPKG')
+        gdf.to_file(gpkg_fpath + '.gpkg', driver='GPKG', layer=f'points_{gpkg_name}')
+
+        # analyze heights if m2 or headallocnoflow in name
+        if 'm2' in gpkg_name or 'headallocnoflow' in gpkg_name:
+            # save a gdf for edges
+            edge_df = df.dropna()
+            edge_df['geometry'] = edge_df['edge_geometry']
+
+            # first step is to remove time for now TODO: later maybe include for animation
+            edge_df = edge_df[edge_df['seconds'] <= 5400]
+            edge_df = edge_df.drop(columns=['time_stamp', 'seconds', 'ACID', 'edge_geometry', 'scenario'])
+
+            # now slice the dataframe into altitude bins
+            alt_bins = [0, 105, 195, 285, 375, 500]
+            
+            final_edge_gdf = deepcopy(edge_gdf)
+            final_edge_gdf = final_edge_gdf.drop(columns=['oneway', 'length', 'bearing', 'stroke_group', 'edge_interior_angle',
+            'layer_allocation', 'bearing_diff', 'height_allocation', 'edge_id', 'flow_group'])
+            for idx, lower_alt in enumerate(alt_bins[:-1]):
+                upper_alt = alt_bins[idx+1]
+                sliced_df = edge_df[((edge_df['ALT'] >= lower_alt) & (edge_df['ALT'] < upper_alt))]
+                edge_counts = sliced_df.groupby(by=['edge_id']).size()
+                sliced_gdf = deepcopy(edge_gdf)
+                sliced_gdf['count'] = sliced_gdf['edge_id'].map(edge_counts)
+                sliced_gdf['count'] = sliced_gdf['count'].fillna(0)
+
+                final_edge_gdf[f'count_bin_{idx}'] = sliced_gdf['count']
+
+            gpkg_fpath = os.path.join('gpkgs', gpkg_name)
+            final_edge_gdf.to_file(gpkg_fpath + '.gpkg', driver='GPKG', layer=f'edge_{gpkg_name}')
+        
 
     except ValueError:
         print('[red]Problem with these files:')
